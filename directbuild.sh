a77828084c305ad6b2ac2e3046c991e5219f6d47
paperbenni <paperbenni@gmail.com>
2020-02-23 16:53:41 +0100

    add repo fail check

--- directbuild.sh
+++ directbuild.sh
@@ -4,6 +4,12 @@ cd
 curl -s https://raw.githubusercontent.com/instantOS/extra/master/directbuild.sh | bash
 cd
 
+# iso build will fail without working repo
+if ! [ -e ~/stuff/extra/build/index.html ]; then
+    echo "error: repository build failed"
+    exit 1
+fi
+
 rm -rf iso-profiles
 git clone --depth=1 https://github.com/instantos/iso-profiles.git
 buildiso -p instantos 2>&1 | tee ~/isobuild
